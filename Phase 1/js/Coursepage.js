cb448da10513b359a3628a953d16b386c60fbb69
Omer-ok2003047 <143384110+Omer-ok2003047@users.noreply.github.com>
2025-03-20 17:12:01 +0300

    Registration html
    creating registration webpage, and working on back-end

--- Phase 1/js/Coursepage.js
+++ Phase 1/js/Coursepage.js
@@ -166,37 +166,3 @@ async function filterCategory(){
         courseBox.append(classDiv);
     });
 }
-// function showBoxes(userType) {
-//     let boxes = document.querySelectorAll('.box');
-//     boxes.forEach(box => box.style.display = 'none')
-
-//     let numToShow = userType === 'student' ? 4 :
-//         (userType === 'teacher' ? 6 : 8);
-
-//     for (let i = 0; i < numToShow; i++) {
-//         boxes[i].style.display = 'block';
-//     }
-// };
-
-// function testFunction() {
-//     console.log("Test function is working!");
-//     alert("Test function is working!");
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-// document.getElementById("sub").addEventListener("click", function () {
-//     showBoxes("student");
-// })
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-// document.getElementById("teacher").addEventListener("click", function () {
-//     showBoxes("teacher");
-// })
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     document.getElementById("testBtn").addEventListener("click",function(){
-//         testFunction();
-//     })
-// });
